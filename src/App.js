06e740a6d7bfb55fc2cbea2b5133e6bbc8eb8497
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-05-29 20:44:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+ 
+ 
 import './App.css';
+import Sidebar from './Sidebar';
+import {selectUser} from "./features/userSlice"
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user= useSelector(selectUser);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      {user ?(
+        <>
+           {/*Sidebar*/}
+      <Sidebar/>
+      {/*Chat*/}
+      </>
+      ):
+      (
+        <h2>You need to login</h2>
+      )}
+      
+   
     </div>
   );
 }
